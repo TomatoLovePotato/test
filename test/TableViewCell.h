8f4f9b1d663d97d52eff62b82c7f3ec027ab7b28
ZhangYang <zyterence@gmail.com>
2014-10-30 23:06:34 +0800

    create TableViewCell class

---
+++ test/TableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  TableViewCell.h
+//  test
+//
+//  Created by zhangyang on 14/10/30.
+//  Copyright (c) 2014年 Yang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TableViewCell : UITableViewCell
+
+@end
---
+++ test/TableViewCell.m
@@ -0,0 +1,23 @@
+//
+//  TableViewCell.m
+//  test
+//
+//  Created by zhangyang on 14/10/30.
+//  Copyright (c) 2014年 Yang. All rights reserved.
+//
+
+#import "TableViewCell.h"
+
+@implementation TableViewCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
